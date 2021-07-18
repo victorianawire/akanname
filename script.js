463df11b65fc2800208c7b534f0bff414d8375e6
Victoria Nawire <victorianawirev@gmail.com>
2021-07-19 00:23:22 +0300

    initialize variables

---
+++ script.js
@@ -0,0 +1,4 @@
+var YY, CC, DD, MM, d, dayValue;
+var dayNames = ["0", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
