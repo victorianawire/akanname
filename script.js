cdd538bc4d29a28a999bae28b8b5074d95b38f78
victorianawire <34067099+victorianawire@users.noreply.github.com>
2021-07-23 09:51:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var YY, CC, DD, MM, d, dayValue;
-var dayNames = ["0", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
