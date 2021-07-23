91fb2915155b9339b4db1a4f4d479d6cf8908181
victorianawire <34067099+victorianawire@users.noreply.github.com>
2021-07-23 10:13:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var YY, CC, DD, MM, d, dayValue;
-var dayNames = ["0", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
 function validate() {
     var genders = document.getElementsByName("gender");
-    if (document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value< 2050 || document.myForm.year.value >= 1800) {
+    if (document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value < 2050 || document.myForm.year.value >= 1800) {
         alert("Please provide a valid year of birth! eg 2019");
         document.myForm.year.focus();
         return false;
