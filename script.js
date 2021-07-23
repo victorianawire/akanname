66b31a861e41245f173f9e3dbc513d2a78035aff
victorianawire <34067099+victorianawire@users.noreply.github.com>
2021-07-23 10:28:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function getGender() {
             }
             break;
         case "female":
-            if (dayValue == 0) {
+            if (dayValue == 1) {
                 alert("You were born on " + dayNames[0] + " and Your akan name is " + maleNames[0] + "!");
             } else if (dayValue == 1) {
                 alert("You were born on " + dayNames[1] + " and Your akan name is " + maleNames[1] + "!");
